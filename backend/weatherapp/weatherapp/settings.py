c8465666cc5403a9151bff3dd99a5903b9040fd6
Vlad <vmusaelyae@gmail.com>
2024-01-27 16:59:21 +0200

    added dockerfiles

--- backend/weatherapp/weatherapp/settings.py
+++ backend/weatherapp/weatherapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
 
 
 # Application definition
