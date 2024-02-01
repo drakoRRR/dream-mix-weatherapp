c8091ee046c61fdc64cff1c2e460836813d0211a
Liiiziiiiii <119292426+Liiiziiiiii@users.noreply.github.com>
2024-02-01 19:43:10 +0200

    update frontend with axios

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,55 +1,112 @@
 import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
+import axios from 'axios';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import WeatherData from './weatherData';
-import { LineChart, xAxis, Tooltip, Label } from '@mui/x-charts/LineChart';
-
+import { LineChart } from '@mui/x-charts/LineChart';
 
 const Home = () => {
     const [loading, setLoading] = useState(true);
     const currentDate = dayjs();
     const [selectedDate, setSelectedDate] = useState(currentDate);
+    const [selectedDropdownCity, setSelectedDropdownCity] = useState('');
+    const [filteredWeather, setFilteredWeather] = useState([]);
+    const [uniqueHours, setUniqueHours] = useState([]);
+    const [chartData, setChartData] = useState([]);
+    const [allCities, setAllCities] = useState([]);
 
+    const cities = ["New York", "Київ", "Харків", "Львів", "Одеса", "Дніпро", "Донецьк", "Запоріжжя", "Луганськ", "Івано-Франківськ", "Чернівці", "Житомир", "Вінниця"];
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
 
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setLoading(false);
-        }, 5000);
-
-        return () => clearTimeout(timeoutId);
-    }, []);
-
-
-    const cities = ["New York", "Київ", "Харків", "Львів", "Одеса", "Дніпро", "Донецьк", "Запоріжжя", "Луганськ", "Івано-Франківськ", "Чернівці", "Житомир", "Вінниця"];
-    const [selectedDropdownCity, setSelectedDropdownCity] = useState('');
-
     const handleCityFilterButtonClick = (selectedCity) => {
         setSelectedDropdownCity(selectedCity);
     };
 
-    const filteredWeather = WeatherData.filter(
-        (weather) =>
-            weather.city === selectedDropdownCity &&
-            dayjs(weather.date).isSame(selectedDate, 'day')
-    );
+    const handleActualForecast = () => {
+        fetchData(selectedDropdownCity, selectedDate);
+    };
+
+    const handleModelForecast = async () => {
+        try {
+            const res = await axios.get('http://localhost:8000/api/get-predict-weather-by-city-date/', {
+                params: {
+                    start_date: selectedDate.format('YYYY-MM-DD'),
+                    end_date: selectedDate.format('YYYY-MM-DD'),
+                    city: selectedDropdownCity,
+                },
+            });
+
+            if (res.status !== 200) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+
+            const predictWeather = res.data;
+            console.log('Predicted Weather:', predictWeather);
+        } catch (error) {
+            console.error('Error fetching predicted weather data:', error);
+        }
+    };
+
+    const fetchData = async (city, date) => {
+        try {
+            const res = await axios.get('http://localhost:8000/api/get-weather-by-city-date/', {
+                params: {
+                    start_date: date.format('YYYY-MM-DD'),
+                    end_date: date.format('YYYY-MM-DD'),
+                    city: city,
+                },
+            });
+
+            if (res.status !== 200) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+
+            const weatherData = res.data;
+            setFilteredWeather(weatherData);
+
+            const updatedUniqueHours = extractUniqueHours(weatherData);
+            const updatedChartData = extractChartData(weatherData, updatedUniqueHours);
+
+            setUniqueHours(updatedUniqueHours);
+            setChartData(updatedChartData);
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const fetchAllCities = async () => {
+        try {
+            const res = await axios.get('http://localhost:8000/api/get-cities');
+
+            if (res.status !== 200) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+
+            const citiesData = res.data;
+            setAllCities(citiesData);
+        } catch (error) {
+            console.error('Error fetching cities data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchAllCities();
+    }, []);
 
     const extractHourFromDateString = (dateString) => {
         const match = dateString.match(/(\d{2}):\d{2}:\d{2}/);
-        if (match) {
-            return match[1];
-        }
-        return null;
+        return match ? match[1] : null;
     };
 
-    const extractUniqueHours = () => {
+    const extractUniqueHours = (weatherData) => {
         const uniqueHours = new Set();
-        filteredWeather.forEach((weather) => {
+        weatherData.forEach((weather) => {
             const hour = extractHourFromDateString(weather.date);
             if (hour !== null) {
                 uniqueHours.add(hour);
@@ -58,22 +115,21 @@ const Home = () => {
         return Array.from(uniqueHours).sort();
     };
 
-    const uniqueHours = extractUniqueHours();
+    const extractChartData = (weatherData, uniqueHours) => {
+        const temperatureData = weatherData.map((weather) => weather.temp_celsius);
 
-    const temperatureData = filteredWeather.map((weather) => weather.temp_celsius);
+        return temperatureData.map((temp, index) => ({
+            x: extractHourFromDateString(weatherData[index].date),
+            y: temp,
+        }));
+    };
 
-    const chartData = temperatureData.map((temp, index) => ({
-        x: extractHourFromDateString(filteredWeather[index].date),
-        y: temp,
-    }));
-
-
-
-
+    useEffect(() => {
+        fetchData(selectedDropdownCity, selectedDate);
+    }, [selectedDropdownCity, selectedDate]);
 
     return (
         <div>
-
             <div style={{ position: 'absolute', top: 10, right: 20 }}>
                 <LocalizationProvider dateAdapter={AdapterDayjs} className="calendar">
                     <DateCalendar
@@ -84,16 +140,14 @@ const Home = () => {
                     />
                 </LocalizationProvider>
             </div>
-            <div class="dropdown">
-                
-
+            <div className="dropdown">
                 <select
                     value={selectedDropdownCity}
                     onChange={(e) => handleCityFilterButtonClick(e.target.value)}
                     className="secondary-button-item_filter"
                 >
                     <option value="">Select city</option>
-                    {cities.map((city, idx) => (
+                    {allCities.map((city, idx) => (
                         <option key={`city-option-${idx}`} value={city}>
                             {city}
                         </option>
@@ -102,17 +156,18 @@ const Home = () => {
 
                 <button
                     className="secondary-button-item"
+                    onClick={handleActualForecast}
                 >
                     Actual forecast
                 </button>
 
                 <button
                     className="secondary-button-item"
+                    onClick={handleModelForecast}
                 >
                     Model forecast
                 </button>
             </div>
-
 
             <div style={{ position: 'relative', width: '500px', height: '300px', marginLeft: '50px' }}>
                 <LineChart
@@ -133,9 +188,7 @@ const Home = () => {
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'absolute', right: 0, bottom: '20px', transform: 'translateX(-50%)' }}>
                     <span style={{ fontWeight: 'bold', transform: 'rotate(90deg)' }}>Hour</span>
                 </div>
-
             </div>
-
 
             <div className="weather_data">
                 <ul className="items-container">
